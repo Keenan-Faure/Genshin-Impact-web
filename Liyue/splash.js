8cf91ab50f9cd18a97767000eed67d801916a921
Keenan-Faure <keenan.asterisk@gmail.com>
2022-02-28 16:34:56 +0200

    update

--- Liyue/splash.js
+++ Liyue/splash.js
@@ -1,4 +1,4 @@
-var splash = document.querySelector('.background');
+var splash = document.querySelector('.splash');
 window.addEventListener('load', (event) =>
 {
     setTimeout(()=>
@@ -9,7 +9,3 @@ window.addEventListener('load', (event) =>
         splash.style.animation = "fadeOut 1s ease-in forwards";
     },6000);
 });
-
-
-//how to disable the web-kit animation using javascript.
-//check if the animation gets cancelled when using the animation=""
