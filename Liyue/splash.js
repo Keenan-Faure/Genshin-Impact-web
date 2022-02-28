6c97a7e2c46da1d2574574e847efa486e24ded02
Keenan-Faure <keenan.asterisk@gmail.com>
2022-02-28 17:00:46 +0200

    update

--- Liyue/splash.js
+++ Liyue/splash.js
@@ -6,6 +6,7 @@ window.addEventListener('load', (event) =>
         console.log(splash);
         splash.style.webkitAnimation = "";
         splash.style.opacity = "1";
+        splash.style.zIndex = "-1";
         splash.style.animation = "fadeOut 1s ease-in forwards";
     },6000);
 });
