2d5ee09b2079cf1549c9b88ae540b6639a8ccad7
Keenan-Faure <keenan.asterisk@gmail.com>
2022-02-28 16:25:34 +0200

    fixed splash screen

--- Liyue/splash.js
+++ Liyue/splash.js
@@ -4,6 +4,12 @@ window.addEventListener('load', (event) =>
     setTimeout(()=>
     {
         console.log(splash);
-        splash.style.animation = "fadeOut 1s ease-out forwards";
+        splash.style.webkitAnimation = "";
+        splash.style.opacity = "1";
+        splash.style.animation = "fadeOut 1s ease-in forwards";
     },6000);
 });
+
+
+//how to disable the web-kit animation using javascript.
+//check if the animation gets cancelled when using the animation=""
