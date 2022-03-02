34bc3f1e86df4a6cc324b967a97d9f2bfadb8742
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-02 12:17:36 +0200

    splash screen fix

--- Liyue/splash.js
+++ Liyue/splash.js
@@ -1,9 +1,19 @@
 var splash = document.querySelector('.splash');
+var splash0 = document.querySelector('.splash0');
+
 window.addEventListener('load', (event) =>
 {
     setTimeout(()=>
     {
-        console.log(splash);
+        splash0.style.animation = "fadeOut 1s ease-in forwards";
+        setTimeout(()=>
+        {
+            splash0.classList.add('display-none');
+        },1500);
+
+    }, 2500);
+    setTimeout(()=>
+    {
         splash.style.webkitAnimation = "";
         splash.style.opacity = "1";
         splash.style.animation = "fadeOut 1s ease-in forwards";
@@ -11,5 +21,5 @@ window.addEventListener('load', (event) =>
         {
             splash.style.zIndex = "-1";
         },1000);
-    },4500);
+    },4000);
 });
