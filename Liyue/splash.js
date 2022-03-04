7a9ef4918657cd7f081e8f06ad59709dd16f1d1d
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-04 14:26:49 +0200

    updated

--- Liyue/splash.js
+++ Liyue/splash.js
@@ -18,7 +18,7 @@ window.addEventListener('load', (event) =>
     //creates splashText
     var SplashText = document.createElement('p');
     SplashText.className = "splashText";
-    var text = document.createTextNode("Will we meet again...");
+    var text = document.createTextNode("Until we meet again...");
     SplashText.appendChild(text);
     Splash1.appendChild(SplashText);
 
