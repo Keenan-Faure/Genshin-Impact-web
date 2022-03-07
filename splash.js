17f078eee4e0460ca78245a522199b5783f08476
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-07 13:05:09 +0200

    updated the tester.html splash

--- splash.js
+++ splash.js
@@ -3,33 +3,46 @@ window.addEventListener('load', (event) =>
 {
     setTimeout(()=>
     {
-        splash.style.animation = "fadeIn 1s ease-out forwards";
-    },3000);
+        //adds image logo to splash screen
+        var image = document.createElement('img');
+        image.id = "center";
+        image.src = 'https://1000logos.net/wp-content/uploads/2021/08/Genshin-Impact-Logo-500x314.png';
+        
+        image.className = 'fade-out 1s ease-in forwards';
+        splash.appendChild(image);
+
+        setTimeout(()=>
+        {
+            console.log("I am here");
+            splash.style.animation = "fadeIn 1s ease-out forwards"
+        },3000);
+        
+    },1000);
     setTimeout(()=>
     {
-        splash.classList.add('display-none');
+        setTimeout(()=>
+        {
+            var btn1 = document.getElementById('btn1');
+            var btn2 = document.getElementById('btn2');
+            var btn3 = document.getElementById('btn3');
+            var btn4 = document.getElementById('btn4');
+            setTimeout(() =>
+            {
+                btn1.style.animation = "fadeOut 1s ease-out forwards";
+            },500);
+            setTimeout(() =>
+            {
+                btn2.style.animation = "fadeOut 1s ease-out forwards";
+            },750);
+            setTimeout(() =>
+            {
+                btn4.style.animation = "fadeOut 1s ease-out forwards";
+            },1000);
+            setTimeout(() =>
+            {
+                btn3.style.animation = "fadeOut 1s ease-out forwards";
+            },1250);
+        },100)
     },5000);
+
 });
-setTimeout(() =>
-{
-    var btn1 = document.getElementById('btn1');
-    var btn2 = document.getElementById('btn2');
-    var btn3 = document.getElementById('btn3');
-    var btn4 = document.getElementById('btn4');
-    setTimeout(() =>
-    {
-        btn1.style.animation = "fadeOut 1s ease-out forwards";
-    },500);
-    setTimeout(() =>
-    {
-        btn2.style.animation = "fadeOut 1s ease-out forwards";
-    },750);
-    setTimeout(() =>
-    {
-        btn4.style.animation = "fadeOut 1s ease-out forwards";
-    },1000);
-    setTimeout(() =>
-    {
-        btn3.style.animation = "fadeOut 1s ease-out forwards";
-    },1250);
-},5000);
