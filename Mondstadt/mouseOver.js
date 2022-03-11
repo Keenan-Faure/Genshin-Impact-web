873e3c462cfc6a3017428209cf32098b9b0565ec
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-11 16:33:43 +0200

    adding Mondstat

---
+++ Mondstadt/mouseOver.js
@@ -0,0 +1,73 @@
+//gets containers
+var contain1 = document.getElementById('contain1');
+var contain2 = document.getElementById('contain2');
+var contain3 = document.getElementById('contain3');
+
+//gets images
+var image1 = document.getElementById('image1');
+var image2 = document.getElementById('image2');
+var image3 = document.getElementById('image3');
+
+contain1.addEventListener('mouseover', (event) =>
+{
+    image1.style.animation = 'fadeIn 1s ease-out forwards'; //make it fade-in quick
+});
+
+contain2.addEventListener('mouseover', (event) =>
+{
+    image2.style.animation = 'fadeIn 1s ease-out forwards'; //make it fade-in quick
+});
+
+contain3.addEventListener('mouseover', (event) =>
+{
+    image3.style.animation = 'fadeIn 1s ease-out forwards'; //make it fade-in quick
+});
+
+
+contain1.addEventListener('mouseout', (event) =>
+{
+    var images = document.getElementsByClassName('btnImage');
+    for(i = 0; i < images.length; ++i)
+    {
+        images[i].style.opacity = '1';
+        images[i].style.animation = 'fadeOut 1s ease-out forwards';
+    }
+});
+
+contain2.addEventListener('mouseout', (event) =>
+{
+    var images = document.getElementsByClassName('btnImage2');
+    for(i = 0; i < images.length; ++i)
+    {
+        images[i].style.opacity = '1';
+        images[i].style.animation = 'fadeOut 1s ease-out forwards';
+        count1 = 1;
+    }
+});
+
+contain3.addEventListener('mouseout', (event) =>
+{
+    var images = document.getElementsByClassName('btnImage3');
+    for(i = 0; i < images.length; ++i)
+    {
+        images[i].style.opacity = '1';
+        images[i].style.animation = 'fadeOut 1s ease-out forwards';
+        count1 = 1;
+    }
+});
+
+/*
+var column1 = document.querySelector('.columnOne');
+var column2 = document.querySelector('.columnTwo');
+var column3 = document.querySelector('.columnThree');
+
+column1.addEventListener('mouseover', (event)=>
+{
+    var darkBack = document.querySelector('.darkBack');
+    darkBack.style.zIndex = '1';
+    darkBack.style.animation = 'fadeIn 1s ease-out forwards';
+    column1.style.animation = 'fadeUp 1s ease-out forwards';
+    
+});
+
+*/
---
+++ Mondstadt/splash.js
@@ -0,0 +1,64 @@
+var splash0 = document.querySelector('.splash0');
+
+window.addEventListener('load', (event) =>
+{
+    setTimeout(()=>
+    {
+        splash0.style.animation = "fadeOut 1s ease-in forwards";
+        setTimeout(()=>
+        {
+            splash0.classList.add('display-none');
+        },1500);
+
+    }, 2500);
+    //creates splash screen...
+    var Splash1 = document.createElement('div');
+    Splash1.className = 'splash';
+
+    //creates splashText
+    var SplashText = document.createElement('p');
+    SplashText.className = "splashText";
+    var text = document.createTextNode("Until we meet again...");
+    SplashText.appendChild(text);
+    Splash1.appendChild(SplashText);
+
+    document.body.appendChild(Splash1);
+    setTimeout(()=>
+    {
+        var splash = document.querySelector('.splash');
+        splash.style.webkitAnimation = "";
+        splash.style.opacity = "1";
+        splash.style.animation = "fadeOut 1s ease-in forwards";
+        setTimeout(() =>
+        {
+            splash.style.zIndex = "-1";
+        },1000);
+    },5000);
+    //The setTimeout method above is directly proportional to the animation seconds
+    //the perfect value for this time is: 5000ms:11s
+
+    setTimeout(()=>
+    {
+        var text1 = document.querySelector('.textContainer');
+        var text2 = document.querySelector('.textContainerTwo');
+        var text3 = document.querySelector('.textContainerThree');
+        var textLast = document.querySelector('.liyueText');
+
+        setTimeout(() =>
+        {
+            text1.style.animation = "fadeIn 1s ease-out forwards";
+        },500);
+        setTimeout(() =>
+        {
+            text2.style.animation = "fadeIn 1s ease-out forwards";
+        },750);
+        setTimeout(() =>
+        {
+            text3.style.animation = "fadeIn 1s ease-out forwards";
+        },1000);
+        setTimeout(()=>
+        {
+            textLast.style.animation = "fadeIn 1s ease-out forwards";
+        },1400)
+    },6200)
+});
