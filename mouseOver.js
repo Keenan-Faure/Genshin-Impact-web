2d424889c19190f31f087a2144c0e37058fd9fab
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-15 16:41:19 +0200

    added the increase animation to all btn's

--- mouseOver.js
+++ mouseOver.js
@@ -1,7 +1,11 @@
 //Mondstadt
 var button = document.querySelector('.mBtn');
-button.addEventListener('onclick', (event)=>
+var button2 = document.querySelector('.mBtn2');
+var button3 = document.querySelector('mBtn3');
+
+button.addEventListener('mouseover', (event)=>
 {
+    button.style.height = "20%";
     button.style.animation = "increase 1s ease-in forwards";
 });
 
@@ -11,6 +15,28 @@ button.addEventListener('mouseout', (event)=>
     button.style.animation = 'decrease 1s ease-in forwards';
 });
 
+button2.addEventListener('mouseover', (event)=>
+{
+    button2.style.height = "20%";
+    button2.style.animation = "increase 1s ease-in forwards";
+});
+
+button2.addEventListener('mouseout', (event)=>
+{
+    button2.style.height = "40%";
+    button2.style.animation = 'decrease 1s ease-in forwards';
+});
+button3.addEventListener('mouseover', (event)=>
+{
+    button3.style.height = "20%";
+    button3.style.animation = "increase 1s ease-in forwards";
+});
+
+button3.addEventListener('mouseout', (event)=>
+{
+    button3.style.height = "40%";
+    button3.style.animation = 'decrease 1s ease-in forwards';
+});
 //Liyue
 
 //Inazuma
