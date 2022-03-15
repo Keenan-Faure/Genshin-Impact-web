005fa9d3436547811a090940528cfe9da4a80592
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-15 17:02:56 +0200

    completed the template of new tester.html

--- mouseOver.js
+++ mouseOver.js
@@ -48,5 +48,102 @@ button3.addEventListener('mouseout', (event)=>
     }, 200);
 });
 //Liyue
+var lbutton = document.querySelector('.lBtn');
+var lbutton2 = document.querySelector('.lBtn2');
+var lbutton3 = document.querySelector('.lBtn3');
+
+lbutton.addEventListener('mouseover', (event)=>
+{
+
+    lbutton.style.animation = "increase 1s ease-in forwards";
+});
+
+lbutton.addEventListener('mouseout', (event)=>
+{
+    lbutton.style.height = "40%";
+    lbutton.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        lbutton.style.height = "20%";
+    },200);
+});
+
+lbutton2.addEventListener('mouseover', (event)=>
+{
+    lbutton2.style.animation = "increase 1s ease-in forwards";
+});
+
+lbutton2.addEventListener('mouseout', (event)=>
+{
+    lbutton2.style.height = "40%";
+    lbutton2.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        lbutton2.style.height = "20%";
+    }, 200);
+});
+lbutton3.addEventListener('mouseover', (event)=>
+{
+    lbutton3.style.animation = "increase 1s ease-in forwards";
+});
+
+lbutton3.addEventListener('mouseout', (event)=>
+{
+    lbutton3.style.height = "40%";
+    lbutton3.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        lbutton3.style.height = "20%";
+    }, 200);
+});
 
 //Inazuma
+
+var ibutton = document.querySelector('.iBtn');
+var ibutton2 = document.querySelector('.iBtn2');
+var ibutton3 = document.querySelector('.iBtn3');
+
+ibutton.addEventListener('mouseover', (event)=>
+{
+
+    ibutton.style.animation = "increase 1s ease-in forwards";
+});
+
+ibutton.addEventListener('mouseout', (event)=>
+{
+    ibutton.style.height = "40%";
+    ibutton.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        ibutton.style.height = "20%";
+    },200);
+});
+
+ibutton2.addEventListener('mouseover', (event)=>
+{
+    ibutton2.style.animation = "increase 1s ease-in forwards";
+});
+
+ibutton2.addEventListener('mouseout', (event)=>
+{
+    ibutton2.style.height = "40%";
+    ibutton2.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        ibutton2.style.height = "20%";
+    }, 200);
+});
+ibutton3.addEventListener('mouseover', (event)=>
+{
+    ibutton3.style.animation = "increase 1s ease-in forwards";
+});
+
+ibutton3.addEventListener('mouseout', (event)=>
+{
+    ibutton3.style.height = "40%";
+    ibutton3.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        ibutton3.style.height = "20%";
+    }, 200);
+});
