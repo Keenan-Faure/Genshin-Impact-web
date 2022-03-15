fa7731c2202740d598dfafe7e0c9571a52c6008a
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-15 16:52:22 +0200

    small fixes

--- mouseOver.js
+++ mouseOver.js
@@ -5,37 +5,47 @@ var button3 = document.querySelector('.mBtn3');
 
 button.addEventListener('mouseover', (event)=>
 {
-    button.style.height = "20%";
+
     button.style.animation = "increase 1s ease-in forwards";
 });
 
 button.addEventListener('mouseout', (event)=>
 {
     button.style.height = "40%";
-    button.style.animation = 'decrease 1s ease-in forwards';
+    button.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        button.style.height = "20%";
+    },200);
 });
 
 button2.addEventListener('mouseover', (event)=>
 {
-    button2.style.height = "20%";
     button2.style.animation = "increase 1s ease-in forwards";
 });
 
 button2.addEventListener('mouseout', (event)=>
 {
     button2.style.height = "40%";
-    button2.style.animation = 'decrease 1s ease-in forwards';
+    button2.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        button2.style.height = "20%";
+    }, 200);
 });
 button3.addEventListener('mouseover', (event)=>
 {
-    button3.style.height = "20%";
     button3.style.animation = "increase 1s ease-in forwards";
 });
 
 button3.addEventListener('mouseout', (event)=>
 {
     button3.style.height = "40%";
-    button3.style.animation = 'decrease 1s ease-in forwards';
+    button3.style.animation = 'decrease 0.2s ease-in forwards';
+    setTimeout(()=>
+    {
+        button3.style.height = "20%";
+    }, 200);
 });
 //Liyue
 
