f9e28de80377739b3684ff640eddaa6f2be34056
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-15 16:19:02 +0200

    added increase/decrease keyframes

---
+++ mouseOver.js
@@ -0,0 +1,11 @@
+var button = document.querySelector('.mBtn');
+button.addEventListener('onclick', (event)=>
+{
+    button.style.animation = "increase 1s ease-in forwards";
+});
+
+button.addEventListener('mouseout', (event)=>
+{
+    button.style.height = "40%";
+    button.style.animation = 'decrease 1s ease-in forwards';
+});
