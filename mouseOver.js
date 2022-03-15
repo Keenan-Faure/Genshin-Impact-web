35887af6e9b89c37dfd7525e25bda216ac440469
Keenan-Faure <keenan.asterisk@gmail.com>
2022-03-15 16:30:47 +0200

    added the cities Slogans

--- mouseOver.js
+++ mouseOver.js
@@ -1,3 +1,4 @@
+//Mondstadt
 var button = document.querySelector('.mBtn');
 button.addEventListener('onclick', (event)=>
 {
@@ -9,3 +10,7 @@ button.addEventListener('mouseout', (event)=>
     button.style.height = "40%";
     button.style.animation = 'decrease 1s ease-in forwards';
 });
+
+//Liyue
+
+//Inazuma
